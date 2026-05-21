670fe753bcce1b721d9a2ed358b3c258d471e92c
test-o-test-o-test-1234 <sanjeevi.v+1234@zohotest.com>
2026-05-21 17:06:59 +0530

    Change response message to include repo name

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
-
+const repo = 'public';
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from the dummy server!');
+  res.end('Hello from the '+repo+' repo server');
 });
 
 const PORT = process.env.PORT || 3000;
